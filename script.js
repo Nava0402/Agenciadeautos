9bf363a8bbf25ae5df6a9e7adfc60e28afa2072e
Nava0402 <al45848@un.edu.mx>
2025-11-18 17:21:35 -0600

    PRODUCTO

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ function calcular() {
     var marcaVehiculo = document.getElementById("marcaVehiculo").value;
 
     var total = 0;
+    
+    if (document.getElementById("contado").checked) {
+        total += 0;
+    } else if (document.getElementById("credito").checked) {
+        total += 0;
+    }
+    else {
+        alert("Seleccione un método de pago");
+        return;
+    }
 
-    if (marcaVehiculo == "HONDA") {
+    if (marcaVehiculo == "NINGUNA") {
+        alert("Seleccione una marca de vehículo");
+        return;
+    } else if(marcaVehiculo == "HONDA") {
         total += 140000;
     } else if (marcaVehiculo == "NISSAN") {
         total += 180000;
@@ -19,6 +32,30 @@ function calcular() {
         total += 160000;
     } else if (marcaVehiculo == "SUZUKI") {
         total += 175000;
+    }
+
+    if (document.getElementById("aireAcondicionado").checked) {
+        total += 3500;
+    }
+
+    if (document.getElementById("gps").checked) {
+        total += 3500;
+    }
+
+    if (document.getElementById("seguro").checked) {
+        total += 3500;
+    }
+
+    if (document.getElementById("quemacocos").checked) {
+        total += 3500;
+    }
+
+    if (document.getElementById("camaraTrasera").checked) {
+        total += 3500;
+    }
+
+    if (document.getElementById("camaraFrontal").checked) {
+        total += 3500;
     }
 
     document.getElementById("total").innerText = "Total: $" + total + " MXN";
